30555731af29aa87e74788af26a9c03e5e995c8f
Simas <simonas.deimantavicius@mediapark.com>
2019-09-16 13:58:29 +0300

    add mock members modal

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+const members = require('./models/Members');
+
+app.get('/api/members', (req, res) => {
+  res.json(members);
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT ||  5000;
 
