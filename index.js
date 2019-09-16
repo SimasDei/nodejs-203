cc3b7fc7609141b529738e2ed45496b3ff94a313
Simas <simonas.deimantavicius@mediapark.com>
2019-09-16 13:50:14 +0300

    serve an HTML page via sendFile method

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Ahoy Sailor o/');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT ||  5000;
